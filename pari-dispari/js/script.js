f648bf9f99c4ebdd7696744d01691b9e6b187359
marcobusacca <marcobusacca01@gmail.com>
2023-05-25 22:08:09 +0200

    [PARI-DISPARI] Alert di Benvenuto

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -12,3 +12,28 @@ function randomNumber(min, max){
     return randomNumber;
 }
 
+// ALERT DI BENVENUTO
+alert(`
+Benvenuto! 
+
+In questa applicazione giocherai contro il Computer!
+
+---------------------------------------------------------------------------
+
+Spiegazione gioco:
+
+Dovrai scegliere tra "PARI" e "DISPARI",
+
+Dopo aver scelto, devi inserire un Numero a tua Scelta compreso tra 1 e 5.
+
+Genereremo un Numero Random (sempre compreso tra 1 e 5) per il Computer, che sarà il tuo avversario!
+
+Se la SOMMA di Entrambi i Numeri sarà un Numero che Corrisponde alla tua Scelta (PARI o DISPARI), Avrai Vinto!
+
+Altrimenti avrai Perso!
+
+---------------------------------------------------------------------------
+
+Clicca OK per continuare, Buona Fortuna!
+`)
+
