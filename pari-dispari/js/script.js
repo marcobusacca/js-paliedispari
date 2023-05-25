bdeb7ebf10879a34c014f7acba4da7a095390446
marcobusacca <marcobusacca01@gmail.com>
2023-05-25 21:59:16 +0200

    [PARI-DISPARI] Funzione randomNumber

--- pari-dispari/js/script.js
+++ pari-dispari/js/script.js
@@ -1,1 +1,14 @@
 "use strict";
+
+// FUNZIONE CHE GENERA UN NUMERO RANDOM
+function randomNumber(min, max){
+
+    // GENERAZIONE NUMERO RANDOM ED INSERIMENTO DEL RISULTATO DENTRO UNA CONSTANTE
+    const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+
+    // STAMPO NELLA CONSOLE IL RISULTATO DELLA GENERAZIONE
+    console.log(randomNumber);
+
+    return randomNumber;
+}
+
